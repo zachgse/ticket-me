473a7001866ead9e6bd3084cc93e8893e2f2b38f
Zach Estrella <zachgabriel.estrella@ziaplex.biz>
2025-08-11 21:08:48 +0800

    Add temporary data table

---
+++ prisma/migrations/20250810190210_add_status_for_tickets/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `ticket` ADD COLUMN `status` VARCHAR(191) NOT NULL DEFAULT 'pending';
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { data:session } = useSession();
-  console.log("session data", session?.user);
+
   return (
     <>
       <div className="flex items-center justify-center mt-24">
